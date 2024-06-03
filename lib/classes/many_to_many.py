840ca0684a0b40e1ce141ee1b25844b4b961b41a
Arnold Kisuri <arnoldkisuri7@gmail.com>
2024-06-03 12:51:16 +0300

    ammending code

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,12 +1,40 @@
 class Article:
+    all = []
     def __init__(self, author, magazine, title):
+        if not isinstance(title,str):
+            raise ValueError('Title must be a string')
+        if not (5 <= len(title) <= 50):
+            raise ValueError('Title must be between 5 and 50 characters')
+        self._title = title
         self.author = author
         self.magazine = magazine
-        self.title = title
+        Article.all.append(self)
+    
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, value):
+        raise AttributeError ('Title is immutable')
+       
         
 class Author:
     def __init__(self, name):
-        self.name = name
+        if not isinstance(name, str):
+            raise ValueError('Name must be a string')
+        if not name:
+            raise ValueError('Name cannot be empty')
+        self._name = name
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        raise AttributeError ('Name is immutable')
+
 
     def articles(self):
         pass
